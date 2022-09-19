140f078d8d0565145c24ae0864ab5eb7342ad019
levtea <rzh2000@163.com>
2022-09-19 10:00:52 +0800

    add contract

--- light/ethsync.go
+++ light/ethsync.go
@@ -2,12 +2,10 @@ package light
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
 
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/ethsync"
 	"github.com/ethereum/go-ethereum/log"
 )
 
@@ -19,11 +17,11 @@ func syncByHeader(lc *LightChain, chain []*types.Header) error {
 		if err != nil {
 			return errors.New(fmt.Sprintf("ankr GetBlockByHash error is %v", err))
 		}
-		body, err := json.Marshal(ethsync.KvBlock(block, lc.GetTd(block.Hash(), block.NumberU64())))
-		if err != nil {
-			log.Error("ankr json Marshal block fail, err is %v", err)
-		}
-		log.Info(fmt.Sprintf("block=%s", string(body)))
+		// body, err := json.Marshal(ethsync.KvBlock(block, lc.GetTd(block.Hash(), block.NumberU64())))
+		// if err != nil {
+		// 	log.Error("ankr json Marshal block fail, err is %v", err)
+		// }
+		// log.Info(fmt.Sprintf("block=%s", string(body)))
 
 		// tx
 		for _, tx := range block.Transactions() {
@@ -37,8 +35,10 @@ func syncByHeader(lc *LightChain, chain []*types.Header) error {
 			return errors.New(fmt.Sprintf("ankr GetBlockReceipts error is %v", err.Error()))
 		}
 		for _, receipt := range receipts {
-			receiptJson, _ := receipt.MarshalJSON()
-			log.Info(fmt.Sprintf("ankrReceipt is %s", string(receiptJson)))
+			// receiptJson, _ := receipt.MarshalJSON()
+			// log.Info(fmt.Sprintf("ankrReceipt is %s", string(receiptJson)))
+
+			log.Info(fmt.Sprintf("ankrReceipt contract is %s", receipt.ContractAddress))
 
 		}
 	}
