c1524d101cb8762b58a9d819d39a6dd8f656fe78
levtea <rzh2000@163.com>
2022-09-19 10:09:10 +0800

    fix

--- light/ethsync.go
+++ light/ethsync.go
@@ -13,10 +13,10 @@ func syncByHeader(lc *LightChain, chain []*types.Header) error {
 	for _, header := range chain {
 		log.Info(fmt.Sprintf("ankr header is %s", header.Number.String()))
 		// block
-		block, err := lc.GetBlockByHash(context.Background(), header.Hash())
-		if err != nil {
-			return errors.New(fmt.Sprintf("ankr GetBlockByHash error is %v", err))
-		}
+		// block, err := lc.GetBlockByHash(context.Background(), header.Hash())
+		// if err != nil {
+		// 	return errors.New(fmt.Sprintf("ankr GetBlockByHash error is %v", err))
+		// }
 		// body, err := json.Marshal(ethsync.KvBlock(block, lc.GetTd(block.Hash(), block.NumberU64())))
 		// if err != nil {
 		// 	log.Error("ankr json Marshal block fail, err is %v", err)
@@ -24,13 +24,25 @@ func syncByHeader(lc *LightChain, chain []*types.Header) error {
 		// log.Info(fmt.Sprintf("block=%s", string(body)))
 
 		// tx
-		for _, tx := range block.Transactions() {
-			txJson, _ := tx.MarshalJSON()
-			log.Info(fmt.Sprintf("ankrTx is %s", string(txJson)))
-		}
+		// for _, tx := range block.Transactions() {
+		// 	txJson, _ := tx.MarshalJSON()
+		// 	log.Info(fmt.Sprintf("ankrTx is %s", string(txJson)))
+		// }
 
 		// receipt
-		receipts, err := GetBlockReceipts(context.Background(), lc.odr, block.Hash(), block.NumberU64())
+		// receipts, err := GetBlockReceipts(context.Background(), lc.odr, block.Hash(), block.NumberU64())
+		// if err != nil {
+		// 	return errors.New(fmt.Sprintf("ankr GetBlockReceipts error is %v", err.Error()))
+		// }
+		// for _, receipt := range receipts {
+		// 	// receiptJson, _ := receipt.MarshalJSON()
+		// 	// log.Info(fmt.Sprintf("ankrReceipt is %s", string(receiptJson)))
+
+		// 	log.Info(fmt.Sprintf("ankrReceipt contract is %s", receipt.ContractAddress))
+
+		// }
+
+		receipts, err := GetBlockReceipts(context.Background(), lc.odr, header.Hash(), header.Number.Uint64())
 		if err != nil {
 			return errors.New(fmt.Sprintf("ankr GetBlockReceipts error is %v", err.Error()))
 		}
