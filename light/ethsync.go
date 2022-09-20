d6a5acbb01950b2b45a7a20a615d924d7444d5a3
levtea <rzh2000@163.com>
2022-09-20 09:37:56 +0800

    log

--- light/ethsync.go
+++ light/ethsync.go
@@ -49,38 +49,30 @@ func syncByHeader(lc *LightChain, chain []*types.Header) error {
 
 		// }
 
-		receipts, err := GetBlockReceiptsSync(context.Background(), lc.odr, header.Hash(), header.Number.Uint64())
+		receipts, err := GetBlockReceipts(context.Background(), lc.odr, header.Hash(), header.Number.Uint64())
 		if err != nil {
 			return errors.New(fmt.Sprintf("ankr GetBlockReceipts error is %v", err.Error()))
 		}
 		for _, receipt := range receipts {
-			// receiptJson, _ := receipt.MarshalJSON()
-			// log.Info(fmt.Sprintf("ankr receipt is %s", string(receiptJson)))
-			// log.Info(fmt.Sprintf("ankr contract_address is %s", receipt.ContractAddress))
+			receiptJson, _ := receipt.MarshalJSON()
+			log.Info(fmt.Sprintf("ankr receipt is %s", string(receiptJson)))
+			log.Info(fmt.Sprintf("ankr contract_address is %s", receipt.ContractAddress))
 
-			if receipt.ContractAddress.String() != EmptyContractAddress {
-				willInsertHeaderSet[header] = struct{}{}
-			}
+			// if receipt.ContractAddress.String() != EmptyContractAddress {
+			// 	willInsertHeaderSet[header] = struct{}{}
+			// }
 		}
-		if len(willInsertHeaderSet) != 0 {
-			for headerInsert := range willInsertHeaderSet {
-				block, err := lc.GetBlockByHash(context.Background(), headerInsert.Hash())
-				if err != nil {
-					return errors.New(fmt.Sprintf("ankr GetBlockByHash error is %v", err))
-				}
-				log.Info(fmt.Sprintf("ankr savedBlock is %s", block.Number()))
-				// body, err := json.Marshal(ethsync.KvBlock(block, lc.GetTd(block.Hash(), block.NumberU64())))
-				// if err != nil {
-				// 	log.Error("ankr json Marshal block fail, err is %v", err)
-				// }
-				// log.Info(fmt.Sprintf("block=%s", string(body)))
+		willInsertHeaderSet[header] = struct{}{}
+	}
+	if len(willInsertHeaderSet) != 0 {
+		for headerInsert := range willInsertHeaderSet {
+			block, err := lc.GetBlockByHash(context.Background(), headerInsert.Hash())
+			log.Info(fmt.Sprintf("ankrBlock save is %v", block.NumberU64()))
 
-				// // tx
-				// for _, tx := range block.Transactions() {
-				// 	txJson, _ := tx.MarshalJSON()
-				// 	log.Info(fmt.Sprintf("ankrTx is %s", string(txJson)))
-				// }
+			if err != nil {
+				return errors.New(fmt.Sprintf("ankr GetBlockByHash error is %v", err))
 			}
+			log.Info(fmt.Sprintf("ankr savedBlock is %s", block.Number()))
 		}
 	}
 	return nil
